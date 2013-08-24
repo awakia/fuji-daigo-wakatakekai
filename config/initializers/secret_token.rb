ad38cba39e766dbdffe92323fb84d0a43ca42cc2
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2013-08-24 22:40:36 +0900

    rails new fuji-daigo-wakatakekai --skip-test-unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FujiDaigoWakatakekai::Application.config.secret_key_base = 'e14e7c075a65aaaed95fcc992a9e10097e2cc7a648c86b16b8f37c03da3c73a0415453ad16b1661e7d5102f3a6963cd4d5e97070c56c27355de5ea8333a01bb9'
