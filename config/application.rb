62f13c3e31259d2584430b578cc935a0f5840838
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2013-11-02 20:11:26 +0900

    I18n for japan

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,10 @@ module FujiDaigoWakatakekai
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = 'Tokyo'
+
+    config.i18n.default_locale = :ja
   end
 end
