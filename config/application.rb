056ddcaf345b96acc6fe544c7ba1d3ba89341c94
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2013-11-02 22:55:02 +0900

    Set up date

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,6 @@ module FujiDaigoWakatakekai
     # config.i18n.default_locale = :de
 
     config.time_zone = 'Tokyo'
-    config.active_record.default_timezone = 'Tokyo'
 
     config.i18n.default_locale = :ja
   end
