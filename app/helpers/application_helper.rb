cf849bc308a6acbbad811fa3d26f3bca173ce364
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2013-11-03 23:52:45 +0900

    Set up new post link from each page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def nl2br(str)
+    return ''.html_safe if str.blank?
+    str = str.split(/\r\n|\n\r|\r|\n/o).map {|line| line }.join('<br>')
+    str.html_safe
+  end
 end
