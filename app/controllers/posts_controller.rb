996b503a1d4f89242e44cf50fa1cd05d1638db21
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2013-11-03 20:11:23 +0900

    Change index order

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.where(published_at: nil) + Post.published.order(:published_at).reverse_order
   end
 
   # GET /posts/1
