2d750177814bcb07d0dc9c298494a2914911d547
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2014-02-15 19:36:07 +0900

    Add Upload.get method

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -4,4 +4,15 @@ class Upload < ActiveRecord::Base
   def image_type?
     name =~ /\.(jpe?g|png|gif|tiff?|bmp)$/i
   end
+
+  def self.get(name, category: nil)
+    scope = Upload.order(:id)
+    res = nil
+    res = scope.where(category: category, name: name).last if category.present?
+    res ||= scope.where(name: name).last
+  end
+
+  def self.get_url(name, category: nil)
+    get(name, category: category).try(:url)
+  end
 end
