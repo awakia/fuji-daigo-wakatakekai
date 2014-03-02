fe55d7fbaa363b87f8bb5957bcd63f04cc37d719
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2014-03-02 16:39:05 +0900

    Set up uploader

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -12,7 +12,11 @@ class FileUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "#{model.class.to_s.underscore.pluralize}/#{mounted_as}/#{model.id}"
+  end
+
+  version :default do
+    process resize_to_limit: [800, 800]
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
