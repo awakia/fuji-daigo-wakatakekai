3a946cb7ec7e5d4ad444587b93069825453553a8
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2014-03-03 01:41:31 +0900

    Set classes automatically

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def main_content_classes(options = {})
+    controller = options[:controller] || params[:controller]
+    action = options[:action] || params[:action]
+    [
+      "#{controller}-controller".gsub(%r(/|_), '-'),
+      "#{controller}-#{canonical_action_name(action)}".gsub(%r(/|_), '-')
+    ].join(' ')
+  end
+
+  def canonical_action_name(action)
+    aliases = { "update" => "edit", "create" => "new" }
+    aliases[action] || action
+  end
 end
