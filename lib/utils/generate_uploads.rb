470d1b34576c767e81c9cfd38ef82561ccf64839
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2014-03-03 04:57:23 +0900

    Add image including utility lib

---
+++ lib/utils/generate_uploads.rb
@@ -0,0 +1,33 @@
+require 'csv'
+
+def process(dirname, category = nil)
+  p "[%s] %s" % [Time.now, dirname]
+  category = nil if category == 'archive'
+  Dir::foreach(dirname) do |item|
+    next if item == '.' || item == '..'
+    full_path = File.join(dirname, item)
+    if File.ftype(full_path) == 'directory'
+      next if item == 'thumbs'
+      process(full_path, category || item)
+    elsif File.extname(item).downcase.in? ['.jpg', '.png', '.pdf']
+      next if item.start_with?('title_')  # skip title images
+      next if item.start_with?('t_')  # skip thumb images
+      name = nil
+      name = 'data_' + item if dirname.end_with?('data') && category == 'info'
+
+      fin = File.open(full_path, 'r')
+      categ = category
+      categ = 'root' if category == 'top'
+      categ += '#' + dirname.split('/')[-2] if category == 'photo_album'
+      Upload.create(file: fin, category: categ, name: name)
+      fin.close
+    end
+  end
+end
+
+process '/Users/aikawa/Desktop/wakatake_140228'
+# filename = Rails.root.join('db', 'uploads.csv')
+# CSV.generate(filename) do |writer|
+#   writer << %w(category name)
+#   rows.each {|row| writer << row }
+# end
